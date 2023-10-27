5989a9dfd839334b061c5d3d801feca9aa805dde
Alejandro Cantarero <cantarer@gmail.com>
2023-10-27 08:57:22 -0700

    enable langchain streaming to gradio ui

--- chatbot/src/chatbot.py
+++ chatbot/src/chatbot.py
@@ -1,23 +1,76 @@
+from queue import SimpleQueue, Empty
+from threading import Thread
+from typing import Any, Union, Dict, List
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.callbacks.manager import CallbackManager
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
+from langchain.schema import LLMResult
+from langchain.chains import LLMChain
+
+q = SimpleQueue()
+job_done = object() # signals the processing is done
+
+class StreamingGradioCallbackHandler(BaseCallbackHandler):
+    def __init__(self, q: SimpleQueue):
+        self.q = q
+
+    def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> None:
+        """Run when LLM starts running. Clean the queue."""
+        while not self.q.empty():
+            try:
+                self.q.get(block=False)
+            except Empty:
+                continue
+
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Run on new LLM token. Only available when streaming is enabled."""
+        self.q.put(token)
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        """Run when LLM ends running."""
+        self.q.put(job_done)
+
+    def on_llm_error(
+        self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
+    ) -> None:
+        """Run when LLM errors."""
+        self.q.put(job_done)
 
 # initialize once
 model = "/models/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
-template = """Question: {question}
-Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+template = """{question}"""
 prompt = PromptTemplate(template=template, input_variables=["question"])
+
+callback_manager = CallbackManager([StreamingGradioCallbackHandler(q)])
 
 llm = LlamaCpp(
     model_path=model,
     temperature=0.75,
     max_tokens=2000,
     top_p=1,
-    verbose=False, 
+    streaming=True,
+    callback_manager=callback_manager, 
+    verbose=True, 
 )
   
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+)
 
-def respond(message, chat_history):
-    output = llm(message)
-    chat_history.append((message, output))
-
-    return "", chat_history
+def respond(history):
+    question = history[-1][0]
+    thread = Thread(target=chain.run, kwargs={"question": question})
+    thread.start()
+    history[-1][1] = ""
+    while True:
+        next_token = q.get(block=True)
+        if next_token is job_done:
+            break
+        history[-1][1] += next_token
+        yield history
+    thread.join()
--- chatbot/src/ui.py
+++ chatbot/src/ui.py
@@ -9,11 +9,16 @@ def feedback(data: gr.LikeData):
     else:
         logger.info("User downvoted the response.")
 
+def user(user_message, history):
+    return "", history + [[user_message, None]]
+
 with gr.Blocks() as chat_app:
     chatbot = gr.Chatbot()
     textbox = gr.Textbox(show_label=False, placeholder="Ask questions to Wikipedia.")
     clear = gr.ClearButton([textbox, chatbot])
     chatbot.like(feedback, None, None)
 
-    textbox.submit(respond, [textbox, chatbot], [textbox, chatbot])
-    textbox.submit(lambda x: gr.update(value=''), [],[textbox])
+    textbox.submit(user, [textbox, chatbot], [textbox, chatbot], queue=False).then(
+        respond, chatbot, chatbot)
+
+    clear.click(lambda: None, None, chatbot, queue=False)
